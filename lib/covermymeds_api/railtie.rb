43618637f87e3e0aa9dcdbc9aac7161ea229cba8
Corey Woodcox <corey.woodcox@gmail.com>
2015-05-08 10:38:13 -0400

    rearrange a little to make these methods more useful
    we actually have a use case for the configured_client method and it
    makes more sense than creating our own client and then calling
    configure_client on it

--- lib/covermymeds_api/railtie.rb
+++ lib/covermymeds_api/railtie.rb
@@ -9,17 +9,17 @@ module CoverMyMeds
     # here and in `Rails.application.config.covermymeds_api`
     def default_client
       return @client if @client
-      @client = CoverMyMeds::Client.new *credentials do |client|
-        configure_client client
-      end
+      @client = configured_client *credentials
     end
 
-    # Configure an API client class with the configuration stored on the app.
-    # Useful if you want to use the same host/path configuration as the rest of
-    # the app, but a different id/secret pair
-    def configure_client(client)
-      config.covermymeds_api.each do |k,v|
-        client.send "#{k}=".to_sym, v
+    # Create a configured API client class with the configuration stored on the
+    # app.  Useful if you want to use the same host/path configuration as the
+    # rest of the app, but a different id/secret pair
+    def configured_client(api_id, secret = nil)
+      CoverMyMeds::Client.new api_id, secret do |client|
+        config.covermymeds_api.each do |k,v|
+          client.send "#{k}=".to_sym, v
+        end
       end
     end
 
