e1d1c48466afd54e5f0ac19f803a616713344bb6
Rachel Twyford <rtwyford@covermymeds.com>
2015-05-12 16:19:46 -0500

    add support for new indicators/search route

--- spec/indicator_spec.rb
+++ spec/indicator_spec.rb
@@ -77,33 +77,41 @@ describe 'Indicators' do
     end
   end
 
-  pending '#post_indicators_search' do
+  describe '#search_indicators' do
     before { VCR.turn_off! }
+    let(:uri) { "https://#{api_id}:#{api_secret}@api.covermymeds.com/indicators/search/?v=#{version}" }
     let(:token_id) { 'faketoken' }
     let!(:api_request) do
       stub_request(:post, uri)
         .with(body: hash_including(post_data))
         .to_return({})
     end
-    let(:post_data) { Hash prescription:  prescription_payload }
+    let(:post_data) { Hash prescriptions:  prescription_payload }
+    let(:prescription_payload) { [{drug_id: '12345'}, {drug_id: '98765'}] }
 
     it 'has prescription data in the request' do
-      client.post_indicators(prescription_payload)
+      client.search_indicators(prescriptions: prescription_payload)
       expect(api_request).to have_been_requested
     end
 
-    context 'when given patient data' do
-      let(:post_data) { Hash prescription: prescription_payload, patient: patient_payload, payer: {} }
+    context 'when prescription data is missing' do
+      it 'raises an error' do
+        expect { client.search_indicators() }.to raise_error(NameError)
+      end
+    end
+
+    context 'when given prescription and patient data' do
+      let(:post_data) { Hash prescriptions: prescription_payload, patient: patient_payload, payer: {} }
       it 'has patient data in the request' do
-        client.post_indicators(prescription_payload, patient_payload)
+        client.search_indicators(prescriptions: prescription_payload, patient: patient_payload)
         expect(api_request).to have_been_requested
       end
     end
 
-    context 'when given payer data' do
-      let(:post_data) { Hash prescription: prescription_payload, patient:  {}, payer: payer_payload }
+    context 'when given prescription and payer data' do
+      let(:post_data) { Hash prescriptions: prescription_payload, patient:  {}, payer: payer_payload }
       it 'has payer data in the request' do
-        client.post_indicators(prescription_payload, {}, payer_payload)
+        client.search_indicators(prescriptions: prescription_payload, patient: {}, payer: payer_payload)
         expect(api_request).to have_been_requested
       end
     end
@@ -111,19 +119,21 @@ describe 'Indicators' do
     context 'when additional data is included in the response body' do 
       #response from indicators API
       let(:extra_data) { Hash pa_required: true, autostart: true }
-      let(:prescription_response) { prescription_payload.merge(extra_data) }
-      let(:post_data) { Hash prescription: prescription_payload, patient:  {}, payer: {} }
+      let(:prescription_response) { prescription_payload.map{ |p| p.merge(extra_data)} }
+      let(:post_data) { Hash prescriptions: prescription_payload, patient:  {}, payer: {} }
 
       before do
         stub_request(:post, uri)
           .with(body: hash_including(post_data))
-          .to_return(status: 200, body: { prescription: prescription_response }.to_json)
+          .to_return(status: 200, body: { prescriptions: prescription_response }.to_json)
       end
 
       it 'includes the additional data' do
-        prediction = client.post_indicators(prescription_payload)
+        prediction = client.search_indicators(prescriptions: prescription_payload)
         extra_data.each do |method_name,value|
-          expect(prediction.prescription.send(method_name)).to eq(value)
+          prediction.prescriptions.each do |p|
+            expect(p.send(method_name)).to eq(value)
+          end
         end
       end
     end
