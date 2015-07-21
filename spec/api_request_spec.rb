51c17658f8bcad06066992f0a8a02e78ec81cbcc
Zach Serafini <serafini.8@osu.edu>
2015-07-21 11:08:59 -0400

    Hand back response body if it can't be JSON parsed

--- spec/api_request_spec.rb
+++ spec/api_request_spec.rb
@@ -1,11 +1,14 @@
 require 'spec_helper'
 
 describe 'Request' do
-  let(:api_id)     { 'ahhbrzs4a0q1om3y7nwn' }
-  let(:api_secret) { 'kkihcug797zu4bzomnh-sbamgqpxyr5yf2pvvqzm' }
-  let(:scheme)     { 'https://' }
-  let(:host)       { 'api.covermymeds.com' }
-  let(:path)       { '/errors' }
+  let(:api_id)            { 'ahhbrzs4a0q1om3y7nwn' }
+  let(:api_secret)        { 'kkihcug797zu4bzomnh-sbamgqpxyr5yf2pvvqzm' }
+  let(:scheme)            { 'https://' }
+  let(:host)              { 'api.covermymeds.com' }
+  let(:path)              { '/errors' }
+  let(:json_response)     { '{ "test": "response" }' }
+  let(:parsed_json)       { JSON.parse(json_response) }
+  let(:non_json_response) { 'This is not JSON' }
 
   let(:client) do
     h, p = "#{scheme}#{host}", path
@@ -16,7 +19,7 @@ describe 'Request' do
     end
   end
 
-  context 'get request' do
+  context 'get request with error' do
     before do
       stub_request(:get, "#{scheme}#{api_id}:#{api_secret}@#{host}#{path}")
       .to_return(status: 400, body: fixture('api_client_error.json'))
@@ -24,6 +27,30 @@ describe 'Request' do
 
     it 'returns single request' do
       expect { client.get_error }.to raise_error(CoverMyMeds::Error::HTTPError)
+    end
+  end
+
+  context 'get request' do
+    context 'non-json response' do
+      before do
+        stub_request(:get, "#{scheme}#{api_id}:#{api_secret}@#{host}#{path}")
+        .to_return(status: 200, body: non_json_response)
+      end
+
+      it "returns the response body if it can't be JSON parsed" do
+        expect(client.request(:get, "#{scheme}#{host}", path)).to eq(non_json_response)
+      end
+    end
+
+    context 'json response' do
+      before do
+        stub_request(:get, "#{scheme}#{api_id}:#{api_secret}@#{host}#{path}")
+        .to_return(status: 200, body: json_response)
+      end
+
+      it "returns the parsed response body" do
+        expect(client.request(:get, "#{scheme}#{host}", path)).to eq(parsed_json)
+      end
     end
   end
 
