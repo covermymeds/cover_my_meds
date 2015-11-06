06545b4b06b2ab44dbe76c42a006dfb5c1bf0ac4
Vasanth Pappu <vpappu@covermymeds.com>
2015-11-06 14:34:55 -0500

    custom error class outputs method and rest resource

--- lib/cover_my_meds/error.rb
+++ lib/cover_my_meds/error.rb
@@ -2,16 +2,20 @@ module CoverMyMeds
   module Error
     class HTTPError < StandardError
 
-      def initialize status, message
+      def initialize status, message, http_method, rest_resource
         @status = status
         @error_json = message
+        @http_method = http_method
+        @rest_resource = rest_resource
       end
 
       def message
-        "#{@status}: #{@error_json}"
+        "#{@status}: #{@error_json}\n"+
+        "in response to:\n"+
+        "#{@http_method.upcase} #{@rest_resource}\n"
       end
 
-      attr_reader :status, :error_json
+      attr_reader :status, :error_json, :http_method, :rest_resource
 
     end
 
