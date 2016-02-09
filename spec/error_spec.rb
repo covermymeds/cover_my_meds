2846cef64350d6b0bd1fbac842c8f42fad758c24
vpappu <vpappu@covermymeds.com>
2016-02-08 23:11:14 -0500

    updated specs

--- spec/error_spec.rb
+++ spec/error_spec.rb
@@ -4,10 +4,14 @@ module CoverMyMeds
   module Error
     describe HTTPError do
 
-      subject { described_class.new status, error_json }
+      subject { described_class.new status, error_json, http_method, rest_resource }
 
       specify "#message" do
-        expect(subject.message).to eq("#{status}: #{error_json}")
+        expect(subject.message).to eq(
+          "#{status}: #{error_json}\n"+
+          "in response to:\n"+
+          "#{http_method.upcase} #{rest_resource}\n"
+        )
       end
 
       specify "#status" do
@@ -18,11 +22,23 @@ module CoverMyMeds
         expect(subject.error_json).to eq(error_json)
       end
 
+      specify "#http_method" do
+        expect(subject.http_method).to eq(http_method)
+      end
+
+      specify "#rest_resource" do
+        expect(subject.rest_resource).to eq(rest_resource)
+      end
+
       let(:status) { 404 }
 
       let(:error_json) do
         Hash[errors: [{ debug: "Nothing to see here, move along." }]].to_json
       end
+
+      let(:http_method) { 'post' }
+
+      let(:rest_resource) { 'https://<url>/<route>/?<params>&v=<version>' }
 
     end
   end
