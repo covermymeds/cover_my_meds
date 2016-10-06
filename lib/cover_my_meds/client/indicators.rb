dfcd7b6c52955cff46498a7a33d2307c92351037
Tom von Schwerdtner <tvon@covermymeds.com>
2016-10-06 13:54:51 -0400

    Address circular argument references
    The addition of parenthesis to the parameter seems to be the recommended
    route.
    
    Via https://bugs.ruby-lang.org/issues/10314#note-4

--- lib/cover_my_meds/client/indicators.rb
+++ lib/cover_my_meds/client/indicators.rb
@@ -4,7 +4,7 @@ module CoverMyMeds
 
     CURRENT_VERSION = 1
 
-    def post_indicators(prescription: prescription, patient: patient, payer: {}, prescriber: {}, version: CURRENT_VERSION)
+    def post_indicators(prescription: prescription(), patient: patient(), payer: {}, prescriber: {}, version: CURRENT_VERSION)
       params = { prescription: prescription, prescriber: prescriber, patient: patient, payer: payer }
       data = indicators_request POST, params: { v: version, headers: { content_type: :json } } do
         params.to_json
@@ -12,7 +12,7 @@ module CoverMyMeds
       Hashie::Mash.new(data)
     end
 
-    def search_indicators(prescriptions: prescriptions, patient: {}, payer: {}, prescriber: {}, version: CURRENT_VERSION)
+    def search_indicators(prescriptions: prescriptions(), patient: {}, payer: {}, prescriber: {}, version: CURRENT_VERSION)
       params = { prescriptions: Array(prescriptions), prescriber: prescriber, patient: patient, payer: payer }
       data = indicators_request POST, path: 'search/', params: { v: version, headers: { content_type: :json } } do
         params.to_json
